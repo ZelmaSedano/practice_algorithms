0b4c24752425e3e97e7c6e80c1409aa23d98305e
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2021-09-13 09:23:14 -0500

    Update 4a_findDuplicates.js

--- 2 - Intermediate/4 - Find Duplicates/4a_findDuplicates.js
+++ 2 - Intermediate/4 - Find Duplicates/4a_findDuplicates.js
@@ -1,17 +1,19 @@
-// create a function that finds the duplicates between two separate arrays
-function findDuplicates(arr) {
-    result = [];
+// find duplicates
+function findDupes(arr) {
+  // create an empty array to holds dupes
+  let result = [];
 
-    // loop through arr1 i times, then arr2 j times (remembering to move one over, so you don't analyze the same element which will return a false-positive) 
-    for(let i = 0; i < arr.length; i++) {
-        for(let j = i+1; j < arr.length; j++) {
-            if(arr[i] === arr[j]) {
-                result.push(arr[i]);
-            }
-        }
+  // loop through the array i times
+  for (let i = 0; i < arr.length; i++) {
+    // loop through the array j times
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] === arr[j]) {
+        result.push(arr[i]);
+      }
     }
+  }
 
-    return result;
+  return result;
 }
 
-console.log(findDuplicates([1,2,3,4,5,1,2,3]));
+console.log(findDupes([1, 2, 3, 4, 1, 2, 3]));
