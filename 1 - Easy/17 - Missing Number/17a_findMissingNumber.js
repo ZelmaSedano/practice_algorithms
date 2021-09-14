0cfa4be259aa77a44d19096b5e37499516754b8d
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2021-09-14 09:03:38 -0500

    Update 17a_findMissingNumber.js

--- 1 - Easy/17 - Missing Number/17a_findMissingNumber.js
+++ 1 - Easy/17 - Missing Number/17a_findMissingNumber.js
@@ -1,13 +1,13 @@
-// create a function that returns the missing number in a series of numbers from an array
-function findMissingNum(arr) {
+// create a function that finds the missing number in an array
+function findMissinNum(arr) {
   // loop through the array
   for (let i = 0; i < arr.length; i++) {
-    // if next num isn't current num + 1, replace it
+    // if current element's next element isn't current element + 1
     if (arr[i + 1] !== arr[i] + 1) {
-      // returns the missing number in its proper place
+      // then return that missing num
       return arr[i] + 1;
     }
   }
 }
 
-console.log(findMissingNum([1, 2, 3, 5]));
+console.log(findMissinNum([1, 2, 4, 5]));
