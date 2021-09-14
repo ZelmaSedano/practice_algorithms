3877f78076cefab82c036a7179593306df2dafc7
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2021-09-14 09:28:04 -0500

    Update 7a_palindrome.js

--- 1 - Easy/7 - Palindrome/7a_palindrome.js
+++ 1 - Easy/7 - Palindrome/7a_palindrome.js
@@ -1,19 +1,9 @@
 // create a function that checks to see if a given string is a palindrome
 function palindrome(str) {
-    // create a variable to hold the reversed string
-    reversed = '';
+  // create reversed version
+  let reversed = str.split('').reverse().join('');
 
-    // loop backwards through the string, pushing each letter into the reversed variable
-    for (let i = str.length-1; i >= 0; i--) {
-        reversed = reversed + str[i];
-    }
-
-    // check to see if the reversed variable and the string are the same; if so return true, otherwise return false
-    if (reversed === str) {
-        return true;
-    } else {
-        return false;
-    }
+  return reversed === str;
 }
 
-console.log(palindrome('won'));
+console.log(palindrome('mow'));
