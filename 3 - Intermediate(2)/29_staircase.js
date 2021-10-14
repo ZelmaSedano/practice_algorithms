278fac4b2a757ad0ecc786bdd7d80425049559c2
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2021-10-14 08:18:10 -0500

    Update 29_staircase.js

--- 3 - Intermediate(2)/29_staircase.js
+++ 3 - Intermediate(2)/29_staircase.js
@@ -1,7 +1,11 @@
+// create a function that creates a staircase out of #'s with the number of steps specified by n
 function staircase(n) {
-  for (let i = 1; i <= n; i++) {
-    console.log("#".repeat(i).padStart(n));
+  // loop from 1 to n, including n
+  for(let i = 1; i <= n; i++) {
+    // console log # and repeat it n times
+    // add padStart(n) - this will push the #'s to the right
+    console.log('#'.repeat(i).padStart(n));
   }
 }
 
-staircase(7);
+staircase(6);
