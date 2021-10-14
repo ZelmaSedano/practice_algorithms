a2b352405b70440bdbcfec7da863f44823da9065
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2021-10-14 06:11:04 -0500

    Update 45_rotateLeft.js

--- 2 - Intermediate/45_rotateLeft.js
+++ 2 - Intermediate/45_rotateLeft.js
@@ -1,8 +1,9 @@
 const rotateLeft = (arr, num) => {
   // slice at d, concat slice from 0 to d onto arr
   // arr.slice(1 arg) = slices off 0 to num
-  let result = arr.slice(num).concat(a.slice(0,num));
+  // slice off 1-3, then add 1-3 to the end of what's left
+  let result = arr.slice(num).concat(arr.slice(0,num));
   return result
 }
 
-console.log()
+console.log(rotateLeft([1,2,3,4,5], 2));
