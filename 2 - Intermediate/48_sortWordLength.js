f336e376a798361595a3037a1a16046e2355a951
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2021-10-17 12:09:01 -0500

    Update 48_sortWordLength.js

--- 2 - Intermediate/48_sortWordLength.js
+++ 2 - Intermediate/48_sortWordLength.js
@@ -1,12 +1,13 @@
 // create a function that sorts words by their length
 function sortWordsLength(str) {
-    // split the str into a list of words
-    let arr = str.split(' ');
+  // split the str into a list of words and remove whitespace
+  let arr = str.match(/[a-zA-Z]+/g);
+  console.log(arr);
 
-    // sort the words by length
-    let sorted = arr.sort((a,b) => a.length-b.length);
+  // sort the words by length
+  let sorted = arr.sort((a, b) => a.length - b.length);
 
-    return sorted;
+  return sorted;
 }
 
-console.log(sortWordsLength('there are a lot of words here'));
+console.log(sortWordsLength('there are      a    lot of words here'));
