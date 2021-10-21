a9ac00742fa156991f7b1ba889efe8cefc48bd8e
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2021-10-21 08:13:34 -0500

    Update 8a_vowel_count.js

--- 2 - Intermediate/8 - Vowel Count/8a_vowel_count.js
+++ 2 - Intermediate/8 - Vowel Count/8a_vowel_count.js
@@ -1,25 +1,21 @@
-// create a function that counts the number of vowels in a given string
+// create a function that counts the number of times a vowel is in a string
 function vowelCount(str) {
-    // identify what a vowel is by creating an array with vowels in it
-    arr = ['a', 'e', 'i', 'o', 'u'];
+    // specify what vowels are
+    let vowels = ['a', 'e', 'i', 'o', 'u'];
 
-    // lowercase all the elements in the string so they match the potential vowels
+    // convert every letter to lowercase so they can match vowels
     str = str.toLowerCase();
 
-    // create a counter variable to hold the number of vowels OUTSIDE the checking loop, because we want the loop to FINISH running so we have the entire results
-    counter = 0;
+    // create a counter variable
+    let count = 0;
 
-    // loop through the string once, then the array of vowels, checking to see if any vowels are present; if there are, add to the counter variable
     for(let i = 0; i < str.length; i++) {
-        for(let j = 0; j < arr.length; j++) {
-            if (str[i] === arr[j]) {
-                counter += 1;
-            }
+        if(vowels.includes(str[i])) {
+            count++;
         }
     }
 
-    // check and return the counter variable for the results
-    return counter;
+    return count;
 }
 
-console.log(vowelCount('what is up my dudes'));
+console.log(vowelCount('hi abc there'));
