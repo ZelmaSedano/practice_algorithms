22645a91a9488d92357bfdae07bf62484eb75f81
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2021-11-02 16:31:59 -0500

    Update 50_checkPassword.js

--- 2 - Intermediate/50_checkPassword.js
+++ 2 - Intermediate/50_checkPassword.js
@@ -1,22 +1,26 @@
-function checkPassword(password) {
-  let arr= [];
-  if (password.search(/[A-Z]/) === -1){
-    arr.push(“UPPER”);
-  }
-  if (password.search(/[a-z]/) === -1){
-    arr.push(“LOWER”);
-  }
-  if (password.search(/[0-9]/) === -1){
-    arr.push(“NUM”);
-  }
-  if (password.search(/[!@#_]/) === -1){
-    arr.push(“SYM”);
-  }
-  if (arr.length === 0){
-    return true;
-  }
-  return arr;
-  }
+// create a function that checks to see if a given password has one capital, one lower-case, one digit, and one symbol in it
+function checkPassword(str) {
+    // create an array to store the missing letters/symbols/digits
+    let result = [];
+
+    // use conditionals to check
+    if(str.search(/[A-Z]/) === -1) {
+        result.push('UPPER');
+    } 
+    if(str.search(/[a-z]/) === -1) {
+        result.push('LOWER');
+    } 
+    if(str.search(/[1-9]/) === -1) {
+        result.push('DIGIT');
+    }
+    if(str.search(/[!@#_]/) === -1) {
+        result.push('SYMBOL');
+    }
+
+    return result;
+}
+
+console.log(checkPassword('Ha1!'));
  // console.log( “Capital check:“, password.search(/[A-Z]/));
  // console.log( “Lowercase check:“, password.search(/[a-z]/));
  // console.log( “Number check:“, password.search(/[0-9]/));
