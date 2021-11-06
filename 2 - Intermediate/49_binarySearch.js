bff82c24dae0e3c9a97f9559a317ae22970f6adc
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2021-11-06 17:21:20 -0500

    Update 49_binarySearch.js

--- 2 - Intermediate/49_binarySearch.js
+++ 2 - Intermediate/49_binarySearch.js
@@ -1,25 +1,27 @@
 // binary search only works w/ sorted arrays
-// return the index of the key/target variable
-function binarySearch(sortedArray, key){
-    let start = 0;
-    let end = sortedArray.length - 1;
+// return the index of the target variable
+function binarySearch(arr, target){
+  let start = 0;
+  let end = arr.length - 1;
 
-    while (start <= end) {
-        let middle = Math.floor((start + end) / 2);
+  while (start <= end) {
+      let middle = Math.floor((start + end) / 2);
 
-        if (sortedArray[middle] === key) {
-            // found the key
-            return middle;
-        } else if (sortedArray[middle] < key) {
-            // continue searching to the right
-            start = middle + 1;
-        } else {
-            // search searching to the left
-            end = middle - 1;
-        }
-    }
-	// key wasn't found
-    return -1;
+      if (arr[middle] === target) {
+          // found the target
+          return 'index: '+ middle;
+        // if middle is less than target, increment start by 1
+      } else if (arr[middle] < target) {
+          // continue searching to the right
+          start = middle + 1;
+        // else decrement end
+      } else {
+          // search searching to the left
+          end = middle - 1;
+      }
+  }
+// target wasn't found
+  return -1;
 }
 
-console.log(binarySearch([1,2,3,4,5,6,7,8,9,10], 4)); // 3, b/c 4 is at index 3
+console.log(binarySearch([1,2,3,4,5,6,7,8,9,10], 1)); // 3, b/c 4 is at index 3
