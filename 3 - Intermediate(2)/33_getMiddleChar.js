e6654c45eca9c5a8e6ac0fd230bfbcda30c7bc6c
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2021-11-11 21:13:31 -0600

    Update 33_getMiddleChar.js

--- 3 - Intermediate(2)/33_getMiddleChar.js
+++ 3 - Intermediate(2)/33_getMiddleChar.js
@@ -1,15 +1,11 @@
-// create a function that returns the middle character of a string
-function getMiddle(str) {
-    // create a variable to hold the middle value of the string length
-    middleIndex = str.length/2;
+function getMiddleChar(str) {
+  let middleIndex = str.length / 2;
 
-    // check to see if the number of elements is even or odd (odd will return a number immediately, but an even number has two middle characters)
-    if (str.length % 2 === 0) {
-        // the .slice() method extracts a section of a string, without modifying the original string
-        return str.slice(middleIndex - 1, middleIndex + 1);
-    } else {
-        return str.charAt(middleIndex);
-    }
+  if (str.length % 2 === 0) {
+    return str.slice(middleIndex - 1, middleIndex + 1); // 2 middle character
+  } else {
+    return str.charAt(middleIndex);
+  }
 }
 
-console.log(getMiddle('now'));
+console.log(getMiddleChar('hih'));
