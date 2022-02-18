4c944b9e5f6042c68b0d9fc7cb226e546b455809
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-02-18 09:19:28 -0600

    Update 2a_two_sum.js

--- 2 - Intermediate/2 - twoSum/2a_two_sum.js
+++ 2 - Intermediate/2 - twoSum/2a_two_sum.js
@@ -1,19 +1,15 @@
-// create a function that sees if any of the numbers from a given array add up to a given num
-function twoSum(arr, num) {
-    // create an empty array to hold the collection of nested arrays that will contain two numbers from the array that add up to num
-    result = [];
-
-    // loop through the array i times, then j times so you can see if any numbers add up to num (make sure to use i+1 as the starter point on the 2nd loop)
+// create a function that finds the two numbers that add up to target
+// this is known as "twosum"
+function twoSum(arr, target) {
+    // loop through the array once, then again, doing i+1
     for(let i = 0; i < arr.length; i++) {
-        for (let j = i+1; j < arr.length; j++) {
-            if (arr[i] + arr[j] === num) {
-                result.push([arr[i], arr[j]]);
-                // if you want to push index, just push i (0) or i+1 (1)
+        for(let j = i+1; j < arr.length; j++) {
+            if(arr[i] + arr[j] === target) {
+                return [arr[i], arr[j]];
             }
         }
     }
-
-    return result;
+    return 'no match';
 }
 
-console.log(twoSum([2, 3, 4, 5], 8));
+console.log(twoSum([1,2,3,4], 5));
