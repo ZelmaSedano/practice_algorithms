58724af857ef5da35011d105698a22a31447cd98
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-02-19 08:56:46 -0600

    Update 3a_check_parentheses.js

--- 1 - Easy/3 - Check Parentheses/3a_check_parentheses.js
+++ 1 - Easy/3 - Check Parentheses/3a_check_parentheses.js
@@ -1,22 +1,17 @@
-// create a function checks to see if a given string has an equal number of opening & closing parentheses
-function checkP(str) {
-    // create a counter variable to count how many times an opening & closing parentheses occurs
-    counter = 0; 
+function checkParentheses(str) {
+    // create a counter variable 
+    let counter = 0; 
 
-    // loop through the string, checking to see if each element (i) is an opening or closing parentheses
+    // loop through the string, checking to see if each element is ( or )
     for(let i = 0; i < str.length; i++) {
-        if (str[i] === '(') {
-            counter += 1;
-        } else if (str[i] === ')') {
-            counter -= 1;
+        if(str[i] === "(") {
+            counter++;
+        } else if(str[i] === ")") {
+            counter--;
         }
     }
 
-    if (counter === 0) {
-        return true;
-    } else {
-        return false;
-    }
+    return counter === 0;
 }
 
-console.log(checkP('()'));
+console.log(checkParentheses('()()('));
