d6ac61ca22fe742bd33c65e2a35ad27e981be4a5
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-02-20 08:33:24 -0600

    Update 5a_1st_non_repeating.js

--- 2 - Intermediate/5 - 1st Non-Repeating Char/5a_1st_non_repeating.js
+++ 2 - Intermediate/5 - 1st Non-Repeating Char/5a_1st_non_repeating.js
@@ -1,24 +1,25 @@
-// create a function that finds the first non-repeating element in a given string
-// you want to loop through the array and while in the loop count how many times each eleemnt is present by comparing w/ an inner j loop
 function firstNonRepeat(str) {
-  // loop through the entire string
-  for (let i = 0; i < str.length; i++) {
-    // create a counter variable
-    let counter = 0;
+    // create an empty object 
+    let obj = {};
 
-    // loop through the string again, without skipping 0
-    for (let j = 0; j < str.length; j++) {
-      if (str[i] === str[j]) {
-        // if the letters match (which they will)
-        counter++; // add to the counter variable
-      } // every letter will have one occurrence, but some will have two
+    // loop through string, if letter isn't in obj, add it; if it is, add to its value by 1
+    for(let char of str) {
+        if(!obj[char]) {
+            obj[char] = 1;
+        } else {
+            obj[char]++;
+        }
     }
 
-    // check the counter while still in the outer loop
-    if (counter === 1) {
-      return str[i];
+    // loop through obj, return first element whose value is 1
+    for(let value in obj) {
+        if(obj[value] === 1) {
+            return 'first non-repeating letter: ', value;
+        }
     }
-  }
+
+    // backup
+    return 'no non-repeating letters';
 }
 
-console.log(firstNonRepeat('hahab')); // b is the first non-repeating element
+console.log(firstNonRepeat('mom'));
