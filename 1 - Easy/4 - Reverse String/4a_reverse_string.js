ca622e452c5eb8ffa5c95775ecef6f8a02c3154e
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-02-22 16:44:18 -0600

    Update 4a_reverse_string.js

--- 1 - Easy/4 - Reverse String/4a_reverse_string.js
+++ 1 - Easy/4 - Reverse String/4a_reverse_string.js
@@ -1,14 +1,5 @@
-//  create a function that returns a given string in reverse
-function reverseString(str) {
-    // create an empty string variable to hold the reversed string
-    result = '';
-
-    // loop backwards from the 3nd of the string, keeping binary counting in mind
-    for(let i = str.length-1; i >= 0; i--) {
-        result += str[i];
-    }
-
-    return result;
+function reverseStr(str) {
+    return str.split('').reverse().join('');
 }
 
-console.log(reverseString('viagra'));
+console.log(reverseStr('hi'))
