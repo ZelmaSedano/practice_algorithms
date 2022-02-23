a6662ec5f686be1652d68580651be5fdf1b62c03
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-02-23 10:40:55 -0600

    Update 10a_word_count.js

--- 1 - Easy/10 - Word Count/10a_word_count.js
+++ 1 - Easy/10 - Word Count/10a_word_count.js
@@ -1,17 +1,15 @@
-// create a function that counts the number of words in a string
 function wordCount(str) {
-    // create a counter variable to hold the count
-    counter = 0; 
-
-    // split the string into an array of words, so we can count them
-    arr = str.split(' ');
-
-    // loop through the array, adding to the counter variable everytime a word is present
-    for(let i = 0; i < arr.length; i++) {
-        counter += 1;
+    if(str.length == 0) {
+        return 'please enter a valid string'
+    }
+    // removes all the empty spaces
+    let arr = str.match(/[a-zA-Z]+/g);
+    if(arr == null) {
+        return 'please enter a valid string'
     }
 
-    return counter;
+    return arr.length;
+
 }
 
-console.log(wordCount('this is a collection of words'));
+console.log(wordCount(' hi'))
