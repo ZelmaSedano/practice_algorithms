4fb0ac6c22445475c47f021e661b3e1302c568d9
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-04-07 12:49:49 -0500

    Update 17a_removeDuplicates.js

--- 2 - Intermediate/17 - Remove Duplicates/17a_removeDuplicates.js
+++ 2 - Intermediate/17 - Remove Duplicates/17a_removeDuplicates.js
@@ -1,7 +1,16 @@
-// create a function that removes the duplicates from an array
-function removeDuplicates(arr) {
-    // wrapping everything in []'s returns the Set as an array
-    return [...new Set(arr)]
+// create a function that finds the common divisors b/w two numbers
+function commonDivisors(num1, num2) {
+    // result array
+    result = []
+
+    // loop from 2 to num2
+    for(let i = 2; i < num2; i++) {
+        if(num1 % i == 0 && num2 % i == 0) {
+            result.push(i)
+        }
+    }
+
+    return [...new Set(result)]
 }
 
-console.log(removeDuplicates([1,2,3,3]))
+console.log(commonDivisors(15,30))
