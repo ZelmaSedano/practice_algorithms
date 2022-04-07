389bb84e872b41346b8b7b7f9d649ed78d2c6d74
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-04-07 17:19:53 -0500

    Update 6a_letter_capitalize.js

--- 2 - Intermediate/7 - Letter Capitalize /6a_letter_capitalize.js
+++ 2 - Intermediate/7 - Letter Capitalize /6a_letter_capitalize.js
@@ -1,15 +1,19 @@
-// create a function that capitalizes the first letter of every word
 function letterCapitalize(str) {
+    // edgecase: if str is empty
+    if(!str || str.length === 0) {
+        return 'please add letters to the string'
+    }
+
     // split the string into an array of words
     arr = str.split(' ');
 
-    // loop through the array of words, targeting the first letter of each word and replacing it with its capitalized version of itself 
-    for (let i = 0; i < arr.length; i++) {
-        arr[i] = arr[i][0].toUpperCase() + arr[i].slice(1);
+    // loop through the array of words, targeting the first letter of each word
+    for(let i = 0; i < arr.length; i++) {
+        // reset the value of the current element to be capitalized and the rest of the word to be lower-case
+        arr[i] = arr[i][0].toUpperCase() + arr[i].slice(1).toLowerCase();
     }
 
-    // return the string with its letters capitalized by joining the array back into a string
     return arr.join(' ');
 }
 
-console.log(letterCapitalize('no way jose'));
+console.log(letterCapitalize('no way jose'))
