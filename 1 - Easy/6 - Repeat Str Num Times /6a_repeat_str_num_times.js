77cefe7ec0c1a2f0b7b0889bc74fa8562e5a5000
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-04-07 12:54:04 -0500

    Update 6a_repeat_str_num_times.js

--- 1 - Easy/6 - Repeat Str Num Times /6a_repeat_str_num_times.js
+++ 1 - Easy/6 - Repeat Str Num Times /6a_repeat_str_num_times.js
@@ -1,20 +1,10 @@
-function capitalizeFirstLetter(str) {
-  // 1) edgecase: if string is empty
-  if(str == '' || str == ' ') {
-    return false
-  }
+function repeatStr(str, num) {
+    // edgecase: if str is empty
+    if(str == '' || str == ' ') {
+        return false
+    }
 
-  // split the string into words
-  words = str.split(' ')
-
-  // loop through the words wordsay
-  for(let i = 0; i < words.length; i++) {
-    // hi --> Hi
-    words[i] = words[i][0].toUpperCase() + words[i].slice(1);
-  }
-
-  // return
-  return words.join(' ')
+    return str.repeat(num)
 }
 
-console.log(capitalizeFirstLetter(''))
+console.log(repeatStr('hi', 3))
