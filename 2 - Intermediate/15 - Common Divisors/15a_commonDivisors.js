d2d11a8a750c05941adf28478322db8aba84d1be
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-04-07 12:51:23 -0500

    Update 15a_commonDivisors.js

--- 2 - Intermediate/15 - Common Divisors/15a_commonDivisors.js
+++ 2 - Intermediate/15 - Common Divisors/15a_commonDivisors.js
@@ -1,34 +1,16 @@
-// create a function that finds the common divisors of two numbers
+// create a function that finds the common divisors b/w two numbers
 function commonDivisors(num1, num2) {
-  // create an empty array to push divisors into for each number
-  let arr1 = [];
-  let arr2 = [];
+    // result array
+    result = []
 
-  // loop from 2 to num for each number, finding divisors and pushing into arrs
-  for(let i = 2; i < num1; i++) {
-    if(num1 % i === 0) {
-      arr1.push(i);
+    // loop from 2 to num2
+    for(let i = 2; i < num2; i++) {
+        if(num1 % i == 0 && num2 % i == 0) {
+            result.push(i)
+        }
     }
-  }
 
-  for(let i = 2; i < num2; i++) {
-    if(num2 % i === 0) {
-      arr2.push(i);
-    }
-  }
-
-  // loop through the two arrays and find the matches
-  let result = [];
-
-  for(let i = 0; i < arr1.length; i++) {
-    for(let j = 0; j < arr2.length; j++) {
-      if(arr1[i] === arr2[j]) {
-        result.push(arr1[i]);
-      }
-    }
-  }
-
-  return result;
+    return [...new Set(result)]
 }
 
-console.log(commonDivisors(15,30));
+console.log(commonDivisors(15,30))
