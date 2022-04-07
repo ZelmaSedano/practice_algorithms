0c2ba5592762811d618baa117d836574f5b31e2e
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-04-07 12:03:20 -0500

    Update 17a_removeDuplicates.js

--- 2 - Intermediate/17 - Remove Duplicates/17a_removeDuplicates.js
+++ 2 - Intermediate/17 - Remove Duplicates/17a_removeDuplicates.js
@@ -1,16 +1,7 @@
-// create a function that removes duplicates from a given array
+// create a function that removes the duplicates from an array
 function removeDuplicates(arr) {
-    // create a variable to hold the new array
-    result = [];
-
-    // using a .forEach iterator, perform a function on each element; if result doesn't include element, add element
-    arr.forEach(function(element) {
-        if(!result.includes(element)) {
-            result.push(element);
-        }
-    })
-
-    return result;
+    // wrapping everything in []'s returns the Set as an array
+    return [...new Set(arr)]
 }
 
-console.log(removeDuplicates([1,1,1,3]));
+console.log(removeDuplicates([1,2,3,3]))
