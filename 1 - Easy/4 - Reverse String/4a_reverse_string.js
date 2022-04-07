85bcbc5a5b743192627561e53044380dfd34ff4c
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-04-07 13:39:59 -0500

    Update 4a_reverse_string.js

--- 1 - Easy/4 - Reverse String/4a_reverse_string.js
+++ 1 - Easy/4 - Reverse String/4a_reverse_string.js
@@ -1,5 +1,10 @@
 function reverseStr(str) {
+    // edgecase: if string is empty
+    if(!str || str.length === 0) {
+        return 'please add letters to string'
+    }
+
     return str.split('').reverse().join('');
 }
 
-console.log(reverseStr('hi'))
+console.log(reverseStr('hi'));
