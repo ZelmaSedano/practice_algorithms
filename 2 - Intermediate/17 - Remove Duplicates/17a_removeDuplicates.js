f084c6daa9b513b8a23df2af8aa98b882373b95e
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-04-08 12:51:42 -0500

    Update 17a_removeDuplicates.js

--- 2 - Intermediate/17 - Remove Duplicates/17a_removeDuplicates.js
+++ 2 - Intermediate/17 - Remove Duplicates/17a_removeDuplicates.js
@@ -1,5 +1,8 @@
 // create a function that returns an array w/ all the duplicates removed
 function removeDuplicates(arr) {
+    if(arr.length === 0) {
+        return 'please add elements to array';
+    }
     return [... new Set(arr)]
 }
 
