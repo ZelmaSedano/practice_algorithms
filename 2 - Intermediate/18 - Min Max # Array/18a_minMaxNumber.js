2faa9a13b1ae2636f47f3691f4d5671052c5957e
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-04-08 12:55:42 -0500

    Update 18a_minMaxNumber.js

--- 2 - Intermediate/18 - Min Max # Array/18a_minMaxNumber.js
+++ 2 - Intermediate/18 - Min Max # Array/18a_minMaxNumber.js
@@ -1,16 +1,13 @@
-// create a function that returns the smallest and largest numbers from a given array in that order (smallest, largest)
 function minMax(arr) {
-    // fail fast - check to see if arr is an array
-    if(arr.isArray === false) {
-        throw new Error('Please enter a valid array');
+    // edgecase: if array is empty
+    if(arr.length === 0) {
+        return 'please add elements to array';
     }
 
-    // sort the array and return first and last element
+    // sort the array 
     let sorted = arr.sort((a,b) => a-b);
-
+    // return the first and last element (min & max)
     return ([sorted[0], sorted[sorted.length-1]]);
 }
 
 console.log(minMax([1,2,3,4,5])) // [1,5]
-console.log(minMax([23334454,5]));
-console.log(minMax([1]));
