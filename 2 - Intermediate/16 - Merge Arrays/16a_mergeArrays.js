e7593d22b05e571d84918d5ee3e850ed9ac5c5cf
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-04-08 12:48:48 -0500

    Update 16a_mergeArrays.js

--- 2 - Intermediate/16 - Merge Arrays/16a_mergeArrays.js
+++ 2 - Intermediate/16 - Merge Arrays/16a_mergeArrays.js
@@ -1,18 +1,10 @@
-// create a function that takes two arrays and merges them together
 function mergeArrays(arr1, arr2) {
-    // create an empty array to hold the new merged one
-    result = [];
-
-    // loop through the two arrays simultaneously
-    for(let i = 0; i < arr1.length || i < arr2.length; i++) {
-        if(i < arr1.length) {  
-            result.push(arr1[i]);
-        } if(i < arr2.length) {
-            result.push(arr2[i]);
-        }
+    // edgecase: if one of the arrays is empty
+    if(arr1.length === 0 || arr2.length === 0) {
+        return 'please add elements to both arrays'
     }
-
+    result = arr1.concat(arr2);
     return result;
 }
 
-console.log(mergeArrays(['a', 'b', 'c'], [1,2,3]));
+console.log(mergeArrays([1,2,3,4], []));
