6be20d2d422e05d0e78dbcd23a82cd09dbc28ef8
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-04-08 10:28:00 -0500

    Update 6a_repeat_str_num_times.js

--- 1 - Easy/6 - Repeat Str Num Times /6a_repeat_str_num_times.js
+++ 1 - Easy/6 - Repeat Str Num Times /6a_repeat_str_num_times.js
@@ -1,10 +1,10 @@
 function repeatStr(str, num) {
     // edgecase: if str is empty
-    if(!str.trim().length) {
+    if(str.trim().length===0) {
         return 'please add letters to string'
     }
 
     return str.repeat(num)
 }
 
-console.log(repeatStr('hi', 3))
+console.log(repeatStr('', 3))
