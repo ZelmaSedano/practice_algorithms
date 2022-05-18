820bc25fd01fcc172e5781b93c3e237050b3818a
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-05-18 09:40:50 -0500

    Update 7a_palindrome.js

--- 1 - Easy/7 - Palindrome/7a_palindrome.js
+++ 1 - Easy/7 - Palindrome/7a_palindrome.js
@@ -1,9 +1,10 @@
 function palindrome(str) {
-    // check length of str
+    // edgecase: if string is empty or spaces
     if(str.trim().length === 0) {
         return 'please add letters to string';
     }
-    return str.split('').reverse().join('');
+
+    return str == str.split('').reverse().join('');
 }
 
-console.log(palindrome(''));
+console.log(palindrome('mom'))
