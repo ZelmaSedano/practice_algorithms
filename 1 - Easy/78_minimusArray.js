ee126a13a12a763bce8e65d9aa227af8b2d2bd04
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-05-18 09:18:41 -0500

    Create 78_minimusArray.js

---
+++ 1 - Easy/78_minimusArray.js
@@ -0,0 +1,12 @@
+function largestNum(arr) {
+    // edgecase: if array is empty
+    if(arr.length === 0) {
+        return 'please add elements to array';
+    }
+
+    // sort the array
+    let sortedArr = arr.sort((a,b) => a-b);
+    return sortedArr[0];
+}
+
+console.log(largestNum([33,3333,3]))
