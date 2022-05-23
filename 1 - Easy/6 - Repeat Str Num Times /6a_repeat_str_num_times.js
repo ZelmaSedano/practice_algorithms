921bbd881d037ce286e99e2d8d41c87d4733c86b
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-05-23 12:47:26 -0500

    Update 6a_repeat_str_num_times.js

--- 1 - Easy/6 - Repeat Str Num Times /6a_repeat_str_num_times.js
+++ 1 - Easy/6 - Repeat Str Num Times /6a_repeat_str_num_times.js
@@ -1,10 +1,14 @@
-function repeatStr(str, num) {
-    // edgecase: if str is empty
-    if(str.trim().length===0) {
-        return 'please add letters to string'
+function repeatString(str, num) {
+    // edgecase: if string is empty or num is negative
+    if(str.trim().length === 0) {
+        return 'please add letters to string';
+    }
+    // if number is negative or not an integer
+    if(num < 0 || num % 1 !== 0) {
+        return 'please only use positive whole numbers';
     }
 
     return str.repeat(num)
 }
 
-console.log(repeatStr('', 3))
+console.log(repeatString('hi', 3))
