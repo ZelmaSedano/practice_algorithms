57072ba2a5c35b39390c301ed897c35974009d9b
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2023-12-10 13:55:43 -0600

    Update 10a_word_count.js

--- 1 - Easy/10 - Word Count/10a_word_count.js
+++ 1 - Easy/10 - Word Count/10a_word_count.js
@@ -1,12 +1,15 @@
 function wordCount(str) {
-    // edgecase: if string is empty or only spaces
-    if(str.trim().length === 0) {
-        return 'please add letters to string';
-    }
+  // edgecase: if string is empty or just spaces
+  if(str.trim().length == 0) {
+    return 'please enter a valid string';
+  }
 
-    // split the string into words, then return the length of the array of words
-    words = str.split(' ');
-    return words.length;
+  // split the string into an array of words
+  // make sure to trim off any trailing spaces
+  let words = str.trim().split(' ');
+  
+  // return the length of the array
+  return words.length;
 }
 
-console.log(wordCount('hi there'));
+console.log(wordCount('hi there b d f '))
