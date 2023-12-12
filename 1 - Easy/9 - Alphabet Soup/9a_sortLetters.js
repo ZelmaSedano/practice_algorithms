3fa30642b781cae0ee6de13a8e130ae72e9678a5
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2023-12-11 18:45:12 -0600

    Update 9a_sortLetters.js

--- 1 - Easy/9 - Alphabet Soup/9a_sortLetters.js
+++ 1 - Easy/9 - Alphabet Soup/9a_sortLetters.js
@@ -1,17 +1,12 @@
-function sortLetters(str) {
+sortLetters = (str) => {
   // edgecase: if string is empty or just spaces
-  if(str.trim().length == 0) {
+  if(str.trim().length== 0) {
     return 'please enter a valid string';
   }
 
-  let letters = [];
-  for(let i = 0; i < str.length; i++) {
-    if(str[i] != ' ') {
-      letters.push(str[i])
-    }
-  }
-
-  return letters.sort().join('');
+  // MAKE SURE TO REMOVE ALL SPACES - split the string into an array of words, this removes the spaces, and join it
+  let newStr = str.split(' ').join('');
+  return newStr.split('').sort().join('');
 }
 
-console.log(sortLetters('how are you'))
+console.log(sortLetters('hi there'))
