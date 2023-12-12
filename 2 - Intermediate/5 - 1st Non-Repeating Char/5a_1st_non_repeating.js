9d9f2f84025a0acd0410a9b0f0f5bdff40d0c728
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2023-12-12 17:28:16 -0600

    Update 5a_1st_non_repeating.js

--- 2 - Intermediate/5 - 1st Non-Repeating Char/5a_1st_non_repeating.js
+++ 2 - Intermediate/5 - 1st Non-Repeating Char/5a_1st_non_repeating.js
@@ -4,11 +4,11 @@ function firstNonRepeatingCharacter(str) {
     return 'please enter a valid string';
   }
   for (let i = 0; i < str.length; i++) {
-    if (str.indexOf(str[i]) == i && str.indexOf(str[i], i + 1) == -1) {
+    if (str.indexOf(str[i], i + 1) == -1) {
       return str[i];
     }
   }
   return "no non-repeating letters";
 }
 
-console.log(firstNonRepeatingCharacter('dkdkdkdsn'))
+console.log(firstNonRepeatingCharacter('adbbac'))
