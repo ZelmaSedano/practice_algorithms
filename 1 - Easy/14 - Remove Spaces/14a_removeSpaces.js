324e3eff22d4b416458d8a76fda17b95c4e74a46
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2023-12-11 18:46:38 -0600

    Update 14a_removeSpaces.js

--- 1 - Easy/14 - Remove Spaces/14a_removeSpaces.js
+++ 1 - Easy/14 - Remove Spaces/14a_removeSpaces.js
@@ -1,12 +1,10 @@
-sortLetters = (str) => {
-  // edgecase: if string is empty or just spaces
-  if(str.trim().length== 0) {
+removeSpaces = (str) => {
+  // if string is empty or just spaces
+  if(str.trim().length == 0) {
     return 'please enter a valid string';
   }
 
-  // MAKE SURE TO REMOVE ALL SPACES - split the string into an array of words, this removes the spaces, and join it
-  let newStr = str.split(' ').join('');
-  return newStr.split('').sort().join('');
+  return str.split(' ').join('');
 }
 
-console.log(sortLetters('hi there'))
+console.log(removeSpaces('hi there'));
