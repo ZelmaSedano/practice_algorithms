7af231345d2441d4eb6822440965740d1caf9685
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2023-12-21 17:44:14 -0600

    Update 5a_1st_non_repeating.js

--- 2 - Intermediate/5 - 1st Non-Repeating Char/5a_1st_non_repeating.js
+++ 2 - Intermediate/5 - 1st Non-Repeating Char/5a_1st_non_repeating.js
@@ -1,14 +1,20 @@
+// The most recent code I've been working on are mostly code challenges
+// I've been trying to optimize them so that they are one-line solutions
 function firstNonRepeatingCharacter(str) {
   // edgecase: if string is empty or just spaces
   if(str.trim().length == 0) {
     return 'please enter a valid string';
   }
+  
   for (let i = 0; i < str.length; i++) {
-    // loop through the string; if the current element's value ISN'T present, return the element's value
+    // loop through the string
+    // if the current char DOESN'T appear more than once, return that character
     if (str.indexOf(str[i], i + 1) == -1) {
       return str[i];
     }
   }
+
+  // backup if there are no non-repeating characters
   return "no non-repeating letters";
 }
 
