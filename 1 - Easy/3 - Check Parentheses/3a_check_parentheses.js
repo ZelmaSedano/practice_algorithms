1587adc9b66ff52ff120e2b1a1b121e1acc434d0
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2024-10-03 07:44:25 -0500

    Update 3a_check_parentheses.js

--- 1 - Easy/3 - Check Parentheses/3a_check_parentheses.js
+++ 1 - Easy/3 - Check Parentheses/3a_check_parentheses.js
@@ -1,25 +1,7 @@
-function checkParentheses(str) {
-    // edgecase: if string is empty or just spaces
-    if(str.trim().length == 0) {
-        return 'please add letters to string';
-    }
+const wordCount = str => {
+  let words = str.trim().split(' ')
 
-    // create a counter variable
-    let count = 0;
-
-    // loop through the string
-    // if the current element is '(', then add to count
-    // if it's ')', then subtract
-    for(let i = 0; i < str.length; i++) {
-        if(str[i] === '(') {
-            count++;
-        } else if(str[i] === ')') {
-            count--;
-        }
-    }
-
-    // if count is zero, return true
-    return count == 0;
+  return words.length;
 }
 
-console.log(checkParentheses(''))
+console.log(wordCount(' hi there'))
